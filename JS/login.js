80b44a1985ec5cdcf6fa6a172ed8998cef785953
lucasfeijodev <lucasfeijodev@gmail.com>
2023-05-02 20:06:20 -0300

    atualização do arquivo js de validação

--- JS/login.js
+++ JS/login.js
@@ -6,9 +6,15 @@ inputUser.addEventListener("focus", ()=>{
 });
 
 inputUser.addEventListener("keyup", ()=>{
+
+    const labelUser = document.querySelector("label[for='idNm']");
+
     if(inputUser.value.length < 5){
         inputUser.setAttribute("style","outline-color:#ff0000;")
+        labelUser.setAttribute("style","color:#ff0000;")
+
     }else{
         inputUser.setAttribute("style","outline-color:#00ff00;")
+        labelUser.setAttribute("style","color:#00ff00;")
     }
-    });
+});
